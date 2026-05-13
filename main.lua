b89ee9a05d808d414048a04a820e39e6ad348f5d
kray312 <brhehrrbo@gmail.com>
2026-05-13 21:15:53 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,1 @@
-Do you really think i am going to give you the loadstring for you to crack it?
-
-nice try bitch!
+print("Do you really think i am going to give you the loadstring for you to crack it? nice try bitch!")
